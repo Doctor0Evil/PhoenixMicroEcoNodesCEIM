4f8e8951ead78385f5a32c44cb0e1e88e99846e0
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2026-01-28 16:05:44 -0700

    Create WetlandKernel.hpp

---
+++ include/WetlandKernel.hpp
@@ -0,0 +1,12 @@
+#ifndef WETLAND_KERNEL_HPP
+#define WETLAND_KERNEL_HPP
+
+#include "WetlandTypes.hpp"
+
+namespace econet {
+
+WetlandResult evaluateWetland(const WetlandConfig& cfg);
+
+} // namespace econet
+
+#endif
